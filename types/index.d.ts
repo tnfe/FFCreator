3f7ab4118e86d33be50a17659ec75e748fc87375
javaSwing <zxdlovejava@gmail.com>
2021-09-01 17:55:02 +0800

    feat: add types test script

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,22 +4,22 @@
 // TypeScript Version: 4.2
 
 
-/// <reference path="FFCreator.d.ts" />
-/// <reference path="FFNode.d.ts" />
-/// <reference path="FFText.d.ts" />
-/// <reference path="FFImage.d.ts" />
-/// <reference path="FFAudio.d.ts" />
-/// <reference path="FFGifImage.d.ts" />
-/// <reference path="FFAnimation.d.ts" />
-/// <reference path="FFAudio.d.ts" />
-/// <reference path="FFVideo.d.ts" />
-/// <reference path="FFAlbum.d.ts" />
-/// <reference path="FFSubtitle.d.ts" />
-/// <reference path="FFScene.d.ts" />
-/// <reference path="FFVtuber.d.ts" />
-/// <reference path="FFLogger.d.ts" />
-/// <reference path="FFTween.d.ts" />
-/// <reference path="FFCreatorCenter.d.ts" />
+/// <reference path="./lib/FFCreator.d.ts" />
+/// <reference path="./lib/FFNode.d.ts" />
+/// <reference path="./lib/FFText.d.ts" />
+/// <reference path="./lib/FFImage.d.ts" />
+/// <reference path="./lib/FFAudio.d.ts" />
+/// <reference path="./lib/FFGifImage.d.ts" />
+/// <reference path="./lib/FFAnimation.d.ts" />
+/// <reference path="./lib/FFAudio.d.ts" />
+/// <reference path="./lib/FFVideo.d.ts" />
+/// <reference path="./lib/FFAlbum.d.ts" />
+/// <reference path="./lib/FFSubtitle.d.ts" />
+/// <reference path="./lib/FFScene.d.ts" />
+/// <reference path="./lib/FFVtuber.d.ts" />
+/// <reference path="./lib/FFLogger.d.ts" />
+/// <reference path="./lib/FFTween.d.ts" />
+/// <reference path="./lib/FFCreatorCenter.d.ts" />
 
 
 
