663a9847be4fce6974a9ed2b5c767b47e33ae02d
drawcall <ajiemath@gmail.com>
2021-09-02 11:31:49 +0800

    feat: add timeline test

--- test/unit/timeline/timeline.test.js
+++ test/unit/timeline/timeline.test.js
@@ -1,20 +1,34 @@
-const Timeline = require('@/timeline/timeline')
+const Timeline = require('@/timeline/timeline');
+const FFScene = require('@/node/scene');
 
-describe('timeline/timeline', ()=> {
-  test('update: Timeline update function ', ()=> {
-    const timeline = new Timeline(60);
-    timeline.annotate(scenes);
-    expect(result).toBeUndefined()
-  })
+describe('timeline/timeline', () => {
+  const createScenes = () => {
+    const rootConf = () => 60;
+    const scene1 = new FFScene();
+    scene1.setDuration(8);
+    scene1.rootConf = rootConf;
+    scene1.setTransition('InvertedPageCurl', 1.5);
 
-  test('addFrameCallback: Add callback hook ', ()=> {
-    timeline.addFrameCallback('func')
-    expect(timeline.cbs.length).toBe(1)
-  })
+    const scene2 = new FFScene();
+    scene2.rootConf = rootConf;
+    scene2.setDuration(10);
 
-  test('removeFrameCallback: remove callback hook ', ()=> {
-    timeline.removeFrameCallback('func')
-    expect(timeline.cbs.length).toBe(0)
-  })
+    return [scene1, scene2];
+  };
 
-})
+  const timeline = new Timeline(60);
+  timeline.annotate(createScenes());
+  test('update: Timeline update function ', () => {
+    expect(timeline.duration).toBe(16.5);
+  });
+
+  test('nextFrame: Next Frame run ', () => {
+    timeline.nextFrame();
+    expect(timeline.frame).toBe(1);
+  });
+
+  test('framesNum: remove callback hook ', () => {
+    timeline.nextFrame();
+    expect(timeline.framesNum).toBe(990);
+  });
+});
