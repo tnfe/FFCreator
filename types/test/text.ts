910d51080adbe03c360e3c20a6b724dedbe08dc6
javaSwing <zxdlovejava@gmail.com>
2021-09-06 22:01:37 +0800

    feat: update text source path

--- types/test/text.ts
+++ types/test/text.ts
@@ -3,15 +3,15 @@ const path = require('path');
 const colors = require('colors');
 
 const createFFTask = () => {
-  const bg1 = path.join(__dirname, './assets/imgs/bg/h04.jpg');
-  const bg2 = path.join(__dirname, './assets/imgs/bg/h05.jpg');
-  const logo1 = path.join(__dirname, './assets/imgs/logo/logo1.png');
-  const logo2 = path.join(__dirname, './assets/imgs/logo/logo2.png');
-  const cover = path.join(__dirname, './assets/imgs/cover/cover2.jpg');
-  const money = path.join(__dirname, './assets/imgs/gif/m.gif');
-  const audio = path.join(__dirname, './assets/audio/02.wav');
-  const font1 = path.join(__dirname, './assets/font/font1.ttf');
-  const font2 = path.join(__dirname, './assets/font/font2.ttf');
+  const bg1 = path.join(__dirname, '../../examples/assets/imgs/bg/h04.jpg');
+  const bg2 = path.join(__dirname, '../../examples/assets/imgs/bg/h05.jpg');
+  const logo1 = path.join(__dirname, '../../examples/assets/imgs/logo/logo1.png');
+  const logo2 = path.join(__dirname, '../../examples/assets/imgs/logo/logo2.png');
+  const cover = path.join(__dirname, '../../examples/assets/imgs/cover/cover2.jpg');
+  const money = path.join(__dirname, '../../examples/assets/imgs/gif/m.gif');
+  const audio = path.join(__dirname, '../../examples/assets/audio/02.wav');
+  const font1 = path.join(__dirname, '../../examples/assets/font/font1.ttf');
+  const font2 = path.join(__dirname, '../../examples/assets/font/font2.ttf');
   const outputDir = path.join(__dirname, './output/');
   const cacheDir = path.join(__dirname, './cache/');
 
