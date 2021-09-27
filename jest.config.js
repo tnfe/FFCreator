f0214dc62f14420246352d8a965703ff3b49dfa4
drawcall <ajiemath@gmail.com>
2021-09-27 11:28:45 +0800

    feat: Modify some configuration options

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
